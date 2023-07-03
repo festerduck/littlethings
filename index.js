765dc1157109bd32f012f6932346b1478ac52055
festerduck <117522051+festerduck@users.noreply.github.com>
2023-07-04 00:57:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+//year divisible by 4 = leap
+//year divisible by 100 != leap
+//year dvisible by 400 = leap
+// function isLeapYear(year)
+// {
+//     var flag = 0;
+//     if(year % 4 === 0)
+//     {
+//         flag = 1;
+//         if(year % 100 === 0)
+//         {
+//             if(year % 400 === 0)
+//             {
+//                 flag = 1;
+//             }
+//             else
+//             {
+//                 flag = 0;   
+//             }
+//         }
+//         if(flag === 1)
+//         {
+//             alert("Leap Year");
+//         }
+//         else
+//         {
+//             alert("Not a Leap Year");
+//         }
+//     }
+//     else
+//     {
+//         alert("Not leap");  
+//     }
+// }
+
+// var year =  prompt("Enter Year: ");
+// isLeapYear(year);
+
